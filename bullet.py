8e405fcff3045f2f2e67b13b583157fee815b5ff
GriffinNelson18 <38507566+GriffinNelson18@users.noreply.github.com>
2018-05-09 13:37:30 -0700

    new file for collisions

---
+++ bullet.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Bullet(pygame.sprite.Sprite):
+    """ This class represents the bullet . """
+    def __init__(self):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+ 
+        self.image = pygame.Surface([4, 10])
+        self.image.fill(BLACK)
+ 
+        self.rect = self.image.get_rect()
+ 
+    def update(self):
+        """ Move the bullet. """
+        self.rect.y -= 3
